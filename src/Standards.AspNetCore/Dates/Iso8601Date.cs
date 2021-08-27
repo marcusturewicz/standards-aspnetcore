228184d020d14d8195a56068c9bf447b016f2044
marcusturewicz <24448509+marcusturewicz@users.noreply.github.com>
2021-08-27 17:34:54 +1000

    Add initial Iso8601Date model binder

---
+++ src/Standards.AspNetCore/Dates/Iso8601Date.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Globalization;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
+
+namespace Standards.AspNetCore
+{
+    public class Iso8601Date : IModelBinder
+    {
+        public Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            var valueProviderResult = bindingContext.ValueProvider.GetValue(bindingContext.ModelName);
+            if (valueProviderResult != ValueProviderResult.None)
+            {
+                bindingContext.ModelState.SetModelValue(bindingContext.ModelName, valueProviderResult);
+
+                var valueAsString = valueProviderResult.FirstValue;
+
+                var dateTimeParsed = DateTime.TryParseExact(valueAsString, "yyyy-MM-dd", CultureInfo.InvariantCulture, DateTimeStyles.None, out var dateTimeResult);
+                if (dateTimeParsed)
+                {
+                    bindingContext.Result = ModelBindingResult.Success(dateTimeResult);
+                }
+                else
+                {
+                    bindingContext.ModelState.AddModelError(bindingContext.ModelName, "Invalid date; must be in ISO-8601 format i.e. YYYY-MM-DD.");
+                }
+            }
+
+            return Task.CompletedTask;
+        }      
+    }
+
+    public class Iso8601DateProvider : IModelBinderProvider
+    {
+        public IModelBinder GetBinder(ModelBinderProviderContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            if (context.Metadata.ModelType == typeof(DateTime))
+            {
+                return new BinderTypeModelBinder(typeof(Iso8601Date));
+            }
+
+            return null;
+        }
+    }      
+}
---
+++ tests/Standards.AspNetCore.Tests.TestServer/Controllers/Iso8601DateController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using Standards.AspNetCore;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Standards.AspNetCore.Tests
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class Iso8601DateController : ControllerBase
+    {
+        [HttpGet("attribute")]
+        public IActionResult GetAttribute([ModelBinder(BinderType = typeof(Iso8601Date))] DateTime date)
+        {
+            return Ok();
+        }
+
+        [HttpGet("provider")]
+        public IActionResult GetProvider(DateTime date)
+        {
+            return Ok();
+        }        
+    }
+}
